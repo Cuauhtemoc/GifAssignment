c7cca54d56e32b0eb21363099b3baef53502dc79
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-01-16 19:07:04 -0600

    added css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,19 @@
 var topics = ["cute dogs", "cute cats", "cute puppies", "cute kittens"];
-
 $(document).on("click","button", function(){
-
     var topic = $(this).attr("data-topic");
-    var url = "https://api.giphy.com/v1/gifs/search?q=" +
+    var url = "https://api.giphy.com/v1/gifs/search?&q=" +
     topic + "&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({
         url: url,
         method: "GET"
     }).then(function(response){
         var results = response.data;
+        console.log(results);
         for (i = 0; i < results.length; i++)
         {
             var topicDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
+            var p1 = $("<p>").text("Rating: " + results[i].rating);
+            var p2 = $("<p>").text("Title: " + results[i].title);
             var topicImage = $("<img>");
             topicImage.attr("src", results[i].images.fixed_height_still.url);
             topicImage.attr("data-still", results[i].images.fixed_height_still.url);
@@ -21,26 +21,24 @@ $(document).on("click","button", function(){
             topicImage.attr("class", "gif");
             topicImage.attr("data-state", "still");
             topicDiv.append(topicImage);
-            topicDiv.append(p);
+            topicDiv.append(p2);
+            topicDiv.append(p1);
             $("#gif-area").append(topicDiv);
         }
     });
 });
-
 $(document).on("click", ".gif", function(){
     var state = $(this).attr("data-state");
     if (state === "still")
     {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
-
     }
     else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
 })
-
 $("#add-gif").on("click", function(event){
     event.preventDefault();
    topics.push($("#new-gif").val());
@@ -53,9 +51,6 @@ function renderButtons(){
         btn.attr("data-topic", topics[i])
         btn.text(topics[i]);
         $("#button-area").append(btn);
-    }
-
-    
+    }    
 }
-
 renderButtons();
